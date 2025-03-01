55f2da09acbe9f0ffbcf8bf1eb13c8fcffa32a0f
Nolan Carroll <10847649@uvu.edu>
2025-03-01 03:12:58 -0700

    Initial commit for What's For Dinner project

---
+++ README.md
@@ -0,0 +1,2 @@
+# This is my basic typescript app for DWDD 2720. 
+Here is the [link to the Netlify deployed page](https://nolan-2720-app.netlify.app/) 
---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+// filepath: /Users/nolan/Documents/web-languages/nolan-2720-app/postcss.config.js
+import tailwindcss from 'tailwindcss';
+
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  }
+}
